d4d559076b472157b0ffe97d0a53cddd66f1dc07
nash-dev <avinashnirmal1@gmail.com>
2022-04-04 08:27:44 +0200

    Working for getting files and splitting  for  Artist and Album

--- FileSort/Program.cs
+++ FileSort/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace FileSort
 {
@@ -10,14 +11,176 @@ namespace FileSort
     {
         public static void Main(string[] args)
         {
+
+          //  List<string> NameOfSongList = new List<string>();
+          //  List<string> AlbumList = new List<string>();
+          //  List<string> ArtistList = new List<string>();
            
+            string folder = ("C:\\Users\\IGENGRAPHICS\\Downloads\\UnSortedMusicFolder");
+            var files = Directory.GetFiles(folder).Min(filename => filename);
+
+            char[] separators = new char[] { '-' };
+
+           // string[] splity = files.Split(new char[] { '.','-', '\n'},
+             //                StringSplitOptions.RemoveEmptyEntries);
+            string[] split = files.Split(new Char[] { '-', '-', '\n' },
+                             StringSplitOptions.RemoveEmptyEntries);
+
+            string[] result = Regex.Split(files, ".-");
+
+
+            string filesToSortByAlbumName = @"-";
+
+            // string[] fileList = System.IO.Directory.GetFiles(folder, filesToSortByAlbumName);
+      
+           var allfiles =  Directory.GetFiles(folder, "*.mp3").Select(fn => new FileInfo(fn)).
+                 OrderBy(f => f.Name);
+
+
+            foreach (var f in allfiles)
+            {
+                string[] splity = f.FullName.Split(new char[] { '.', '-', '\n' },
+                     StringSplitOptions.RemoveEmptyEntries);
+
+                var NameOfSongList = splity[0];
+                Console.WriteLine(NameOfSongList);
+
+                var Artist = splity[1];
+                Console.WriteLine(Artist);
+
+
+                var Album = splity[2];
+                Console.WriteLine(Album);
+
+
+
+                Console.WriteLine(splity);
+
+
+                //Console.WriteLine($"Substring: {f}");
+            }
+            /*
+            foreach(var f in fileList)
+            {
+        
+            }
+
+            foreach (var elem in ((IEnumerable)fileList[1]).Cast<>())
+            {
+                var id = elem.Id;
+                var name = elem.Names;
+            }
+
+
+              
+            
+
+            foreach (var file in files)
+            {
+
+                string[] split = files.Split(new Char[] { '-', '-', '\n' },
+                                 StringSplitOptions.RemoveEmptyEntries);
+                
+
+            }
+
+
+                    foreach (var file in files)
+            {
+                char[] separators = new char[] { '-' };
+                string[] subs = files.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var sub in subs)
+                {
+                    Console.WriteLine($"Substring: {sub}");
+                }
+                Console.WriteLine(file);
+            }
+
+
+            foreach (var elem in ((IEnumerable)classArray[1]).Cast<ExcelSDRAddIn.UserControlSDR.Classification>())
+            {
+                var id = elem.Id;
+                var name = elem.Names;
+            }
+            // string s = "You win some. You lose some.";
+
+
+            string[] fileList = System.IO.Directory.GetFiles(folder);
+
+            /*
+
+
+            string path1 = @"c:\temp\MyTest.txt";
+            string path2 = @"c:\temp\MyTest";
+            string path3 = @"temp";
+
+            if (Path.HasExtension(path1))
+            {
+                Console.WriteLine("{0} has an extension.", path1);
+            }
+
+            if (!Path.HasExtension(path2))
+            {
+                Console.WriteLine("{0} has no extension.", path2);
+            }
+
+            if (!Path.IsPathRooted(path3))
+            {
+                Console.WriteLine("The string {0} contains no root information.", path3);
+            }
+
+            Console.WriteLine("The full path of {0} is {1}.", path3, Path.GetFullPath(path3));
+            Console.WriteLine("{0} is the location for temporary files.", Path.GetTempPath());
+            Console.WriteLine("{0} is a file available for use.", Path.GetTempFileName());
+
+
+
+
+
+
+            // taking full path of a file
+            string strPath = "C:// myfiles//ref//file1.txt";
+
+            // initialize the value of filename
+            string filename = null;
+
+            // using the method
+            filename = Path.GetFileNameWithoutExtension(strPath);
+            Console.WriteLine("Filename = " + filename);
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             string originalFiles = ("\\Downloads\\UnSortedTest");
 
             string filesToSortByAlbumName = @"-";
+
+            List<string> names { get; set; }
+
             string[] fileList = System.IO.Directory.GetFiles(originalFiles, filesToSortByAlbumName, SearchOption.TopDirectoryOnly);
 
+            string[] files = Directory.GetFiles(originalFiles).OrderBy(f => int.Parse(Path.GetFileNameWithoutExtension(f))).ToArray();
+
+
             string destinationPathArtistNamesFolder = "\\Downloads\\UnSortedTest\\Artist";
+
+
+            DirectoryInfo di = Directory.CreateDirectory(destinationPathArtistNamesFolder);
+            Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(destinationPathArtistNamesFolder));
+
             {
                 string[] input = {
                                  "NameOfSong 0",
@@ -37,6 +200,8 @@ namespace FileSort
                 this.name = name;
                 names = name.Split(new char[] { '-' }).ToList();
             }
+
+            
             public int CompareTo(HyphenString other)
             {
                 int results = 0;
@@ -51,8 +216,9 @@ namespace FileSort
 
                 return names.Count.CompareTo(other.names.Count); ;
             }
-
+            
         }
+                 
 
         /*
            string originalFiles = ("C:\\Users\\IGENGRAPHICS\\Downloads\\UnSortedTest");
@@ -127,8 +293,10 @@ namespace FileSort
            Console.ReadLine();
            Console.WriteLine("Hello World!");
            */
+            /*
 
+            */
 
-
+        }
     }
 }
